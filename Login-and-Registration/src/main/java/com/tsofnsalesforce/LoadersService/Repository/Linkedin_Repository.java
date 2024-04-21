5a9b8d6913101e0ef538fdee667afa6a2ff46b70
zain zoabi <82890692+zainzoabi98@users.noreply.github.com>
2024-04-21 20:14:18 +0300

    fix #1 loaders service

--- Login-and-Registration/src/main/java/com/tsofnsalesforce/LoadersService/Repository/Linkedin_Repository.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Linkedin_Repository extends JpaRepository<LinkedinPost, Long> {
-}
