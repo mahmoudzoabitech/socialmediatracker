3c7b75b4167dc4b8f91920e5b4bbb410d8d0d8d0
mahmoudzoabitech <mahmoudzoabitech@gmail.com>
2024-05-18 19:46:09 +0300

    fix mail

--- Login-and-Registration/src/main/java/com/tsofnsalesforce/LoginandRegistration/config/SecurityConfiguration.java
+++ Login-and-Registration/src/main/java/com/tsofnsalesforce/LoginandRegistration/config/SecurityConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -23,43 +22,43 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> {
-                    request.requestMatchers("/api/v1/authenticate/**").permitAll();
-                    request.requestMatchers("/api/v1/auth/**").hasAuthority("ADMIN");
-                    request.anyRequest().authenticated();
-                        })
-//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return  http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//                .cors(withDefaults())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(request -> {
+//                    request.requestMatchers("/api/v1/authenticate/**").permitAll();
+//                    request.requestMatchers("/api/v1/auth/**").hasAuthority("ADMIN");
+//                    request.anyRequest().authenticated();
+//                        })
+////                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+//                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//        return  http.build();
+//    }
 
 //
-//@Bean
-//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//    http
-////            .cors(withDefaults())
-//            .csrf(AbstractHttpConfigurer::disable)
-//            .authorizeHttpRequests(request -> {
-//                request.requestMatchers("/api/v1/authenticate/**",
-//                                                    "/api/v1/auth/**"
-//                        )
-//                        .permitAll()
-//                        .anyRequest()
-//                        .authenticated();
-//            })
-////            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-//            .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-//            .authenticationProvider(authenticationProvider)
-//            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//    return  http.build();
-//}
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http
+//            .cors(withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> {
+                request.requestMatchers("/api/v1/authenticate/**",
+                                                    "/api/v1/auth/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated();
+            })
+//            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+            .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    return  http.build();
+}
 }
